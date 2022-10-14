11270e49e27748e0d80528d468d56d53d339d2b4
“sagar <sagar.prusty@siemens.com>
2022-10-14 11:58:38 +0530

    Initial check in

---
+++ deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "D:/UserData/z00474te/Documents/Mendix/AzurePOC-ResourceManagerChanges/themesource/administration/native/main";
+import * as style1 from "D:/UserData/z00474te/Documents/Mendix/AzurePOC-ResourceManagerChanges/themesource/nanoflowcommons/native/main";
+import * as style2 from "D:/UserData/z00474te/Documents/Mendix/AzurePOC-ResourceManagerChanges/themesource/webactions/native/main";
+import * as style3 from "D:/UserData/z00474te/Documents/Mendix/AzurePOC-ResourceManagerChanges/themesource/atlas_core/native/main";
+import * as style4 from "D:/UserData/z00474te/Documents/Mendix/AzurePOC-ResourceManagerChanges/themesource/azureextensionmodule/native/main";
+import * as style5 from "D:/UserData/z00474te/Documents/Mendix/AzurePOC-ResourceManagerChanges/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
